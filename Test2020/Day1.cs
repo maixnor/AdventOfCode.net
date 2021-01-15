1c0f2e858c358288db44738da899b2dd309049fb
notThere0815 <klappstuhl@schoschonen.at>
2021-01-15 10:31:38 +0100

    Input.txt copy to output folder paths correct

--- Test2020/Day1.cs
+++ Test2020/Day1.cs
@@ -11,5 +11,11 @@ namespace Test2020
             Assert.True(Challenge.SumTo2020(1721, 299));
             Assert.False(Challenge.SumTo2020(1720, 299));
         }
+
+        [Fact]
+        public void EnsureInput()
+        {
+            Assert.NotEmpty(Challenge.GetInput());
+        }
     }
 }
--- Year2020/Day1/Challenge.cs
+++ Year2020/Day1/Challenge.cs
@@ -1,10 +1,18 @@
-﻿namespace Year2020.Day1
+﻿using System.IO;
+
+namespace Year2020.Day1
 {
     public class Challenge
     {
         public static bool SumTo2020(int x, int y)
         {
             return x + y == 2020;
+        }
+
+        public static string[] GetInput()
+        {
+            var lines = File.ReadAllLines("Day1/Input.txt");
+            return lines;
         }
     }
 }

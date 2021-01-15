f9fdb1603628b1e0d17cc63fed4fc8eb2494e810
notThere0815 <klappstuhl@schoschonen.at>
2021-01-15 09:59:58 +0100

    adds up to 2020

---
+++ Test2020/Day1.cs
@@ -0,0 +1,15 @@
+using Xunit;
+using Year2020.Day1;
+
+namespace Test2020
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void EnsureAddsUpTo2020()
+        {
+            Assert.True(Challenge.SumTo2020(1721, 299));
+            Assert.False(Challenge.SumTo2020(1720, 299));
+        }
+    }
+}
---
+++ Year2020/Day1/Challenge.cs
@@ -0,0 +1,10 @@
+﻿namespace Year2020.Day1
+{
+    public class Challenge
+    {
+        public static bool SumTo2020(int x, int y)
+        {
+            return x + y == 2020;
+        }
+    }
+}

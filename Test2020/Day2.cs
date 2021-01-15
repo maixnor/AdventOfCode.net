e830dcd004258918f5aa8624e2be92afaf414344
notThere0815 <klappstuhl@schoschonen.at>
2021-01-15 12:06:43 +0100

    all test logic

--- Test2020/Day2.cs
+++ Test2020/Day2.cs
@@ -5,6 +5,47 @@ namespace Test2020
 {
     public class Day2
     {
+
+        private string TestLine = "1-3 a: abcde";
+
+        [Fact]
+        public void EnsureFrom()
+        {
+            Assert.Equal(1, Challenge.GetFrom(TestLine));
+        }
+        
+        [Fact]
+        public void EnsureTo()
+        {
+            Assert.Equal(3, Challenge.GetTo(TestLine));
+        }
+
+        [Fact]
+        public void EnsureCharacter()
+        {
+            Assert.Equal('a', Challenge.GetCharacter(TestLine));
+        }
+
+        [Fact]
+        public void EnsurePassword()
+        {
+            Assert.Equal("abcde", Challenge.GetPassword(TestLine));
+        }
+
+        [Fact]
+        public void EnsureOccurrences()
+        {
+            Assert.Equal(1,Challenge.Occurrences('a', Challenge.GetPassword(TestLine)));
+        }
+
+        [Fact]
+        public void EnsureValid()
+        {
+            Assert.True(Challenge.IsValid("1-3 a: abcde"));
+            Assert.False(Challenge.IsValid("1-3 b: cdefg"));
+            Assert.True(Challenge.IsValid("2-9 c: ccccccccc"));
+        }
+        
         [Fact]
         public void EnsureInput()
         {
--- Year2020/Day2/Challenge.cs
+++ Year2020/Day2/Challenge.cs
@@ -1,15 +1,48 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Year2020.Day2
 {
-    public class Challenge
+    public static class Challenge
     {
-
 
         public static string[] GetInput()
         {
             var lines = File.ReadAllLines("Day2/Input.txt");
             return lines;
-        } 
+        }
+
+        public static int GetFrom(string line)
+        {
+            return int.Parse(line.Substring(0, line.IndexOf('-')));
+        }
+
+        public static int GetTo(string line)
+        {
+            var startIndex = line.IndexOf('-');
+            return int.Parse(line.Substring(startIndex + 1, line.IndexOf(' ') - startIndex));
+        }
+
+        public static char GetCharacter(string line)
+        {
+            return line[line.IndexOf(':') - 1];
+        }
+
+        public static string GetPassword(string line)
+        {
+            return line.Substring(line.IndexOf(':') + 2);
+        }
+
+        public static int Occurrences(char character, string password)
+        {
+            return password.Count(letter => character == letter);
+        }
+
+        public static bool IsValid(string line)
+        {
+            var occurrences = Occurrences(GetCharacter(line), GetPassword(line));
+            return GetFrom(line) <= occurrences && occurrences <= GetTo(line);
+        }
     }
 }

62747b39880bf75fa49f8fc7b53ddf7fc03dbe24
notThere0815 <klappstuhl@schoschonen.at>
2021-01-15 13:33:09 +0100

    day 3, part 1 one

--- Test2020/Day3.cs
+++ Test2020/Day3.cs
@@ -1,7 +1,40 @@
-﻿namespace Test2020
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+using Year2020.Day3;
+
+namespace Test2020
 {
     public class Day3
     {
+        private string TestLine = "..##.......";
+
+        [Fact]
+        public void EnsureInput()
+        {
+            Assert.NotEmpty(Challenge.GetInput());
+        }
+
+        [Fact]
+        public void EnsureIsHit()
+        {
+            Assert.False(Challenge.IsTree(0, TestLine));
+            Assert.True(Challenge.IsTree(2, TestLine));
+            Assert.False(Challenge.IsTree(11, TestLine)); // + 11
+            Assert.True(Challenge.IsTree(13, TestLine));  // + 11
+        }
+
+        [Fact]
+        public void FindResult()
+        {
+            _testOutputHelper.WriteLine(Challenge.FindResult().ToString());
+        }
         
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public Day3(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
     }
 }
--- Year2020/Day3/Challenge.cs
+++ Year2020/Day3/Challenge.cs
@@ -1,13 +1,29 @@
 ﻿using System.IO;
-using System.Linq;
 
 namespace Year2020.Day3
 {
-    public class Challenge
+    public static class Challenge
     {
         public static string[] GetInput()
         {
             return File.ReadAllLines("Day3/Input.txt");
+        }
+
+        public static int FindResult()
+        {
+            var lines = GetInput();
+            var counter = 0;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (IsTree(i * 3, lines[i]))
+                    counter++;
+            }
+            return counter;
+        }
+
+        public static bool IsTree(int idx, string line)
+        {
+            return line[idx % line.Length] == '#';
         }
     }
 }

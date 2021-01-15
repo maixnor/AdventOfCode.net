5c7e4937892026d93522c42c14aa070efbfebdd6
notThere0815 <klappstuhl@schoschonen.at>
2021-01-15 11:29:43 +0100

    input PreserveNewest

--- Test2020/Day2.cs
+++ Test2020/Day2.cs
@@ -1,7 +1,14 @@
-﻿namespace Test2020
+﻿using Xunit;
+using Year2020.Day2;
+
+namespace Test2020
 {
     public class Day2
     {
-        
+        [Fact]
+        public void EnsureInput()
+        {
+            Assert.NotEmpty(Challenge.GetInput());            
+        }
     }
 }
--- Year2020/Day2/Challenge.cs
+++ Year2020/Day2/Challenge.cs
@@ -1,7 +1,15 @@
-﻿namespace Year2020.Day2
+﻿using System.IO;
+
+namespace Year2020.Day2
 {
     public class Challenge
     {
-        
+
+
+        public static string[] GetInput()
+        {
+            var lines = File.ReadAllLines("Day2/Input.txt");
+            return lines;
+        } 
     }
 }

26e95c846fea59b237f2134010d0a7b62d9f4107
notThere0815 <klappstuhl@schoschonen.at>
2021-01-15 13:17:20 +0100

    input and getInput refactorings

--- Year2020/Day3/Challenge.cs
+++ Year2020/Day3/Challenge.cs
@@ -1,7 +1,13 @@
-﻿namespace Year2020.Day3
+﻿using System.IO;
+using System.Linq;
+
+namespace Year2020.Day3
 {
     public class Challenge
     {
-        
+        public static string[] GetInput()
+        {
+            return File.ReadAllLines("Day3/Input.txt");
+        }
     }
 }

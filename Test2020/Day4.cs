1e8ab50154e9ff22e329cb05999cdc9b07ef753d
notThere0815 <klappstuhl@schoschonen.at>
2021-01-15 15:25:00 +0100

    still error in ParsePassport

--- Test2020/Day4.cs
+++ Test2020/Day4.cs
@@ -1,7 +1,91 @@
-﻿namespace Test2020
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+using Year2020.Day4;
+
+namespace Test2020
 {
     public class Day4
     {
+        [Fact]
+        public void FindSemiValidPassports()
+        {
+            _testOutputHelper.WriteLine(Challenge.FindSemiValidCount().ToString());
+        }
         
+        [Fact]
+        public void EnsureInput()
+        {
+            Assert.NotNull(Challenge.GetData());
+        }
+
+        [Fact]
+        public void EnsureParseAllPassports()
+        {
+            Assert.Equal(281, Challenge.ParseAllPassports().Length);
+        }
+
+        [Fact]
+        public void EnsureParsePassport()
+        {
+            var passport = new Passport
+            {
+                Byr = "gry",
+                Pid = "860033327",
+                Eyr = "2020",
+                Hcl = "#fffffd",
+                Ecl = "1937",
+                Iyr = "2017",
+                Hgt = "183cm",
+                Cid = "147"
+            };
+            var passportFromChallenge = Challenge.ParsePassport(new[]
+            {
+                "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd",
+                "byr:1937 iyr:2017 cid:147 hgt:183cm"
+            });
+            Assert.Equal(passport, passportFromChallenge);
+        }
+
+        [Fact]
+        public void EnsurePassportValid()
+        {
+            var passport = new Passport
+            {
+                Byr = "gry",
+                Pid = "860033327",
+                Eyr = "2020",
+                Hcl = "#fffffd",
+                Ecl = "1937",
+                Iyr = "2017",
+                Hgt = "183cm"
+            };
+            Assert.False(passport.IsFullyValid());
+            passport.Cid = "147";
+            Assert.True(passport.IsFullyValid());
+        }
+
+        [Fact]
+        public void EnsurePassportSemiValid()
+        {
+            var passport = new Passport
+            {
+                Byr = "gry",
+                Pid = "860033327",
+                Eyr = "2020",
+                Hcl = "#fffffd",
+                Ecl = "1937",
+                Iyr = "2017",
+                Hgt = "183cm"
+            };
+            Assert.True(passport.IsValid());
+        }
+        
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public Day4(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
     }
 }
--- Year2020/Day4/Challenge.cs
+++ Year2020/Day4/Challenge.cs
@@ -1,10 +1,88 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Year2020.Day4
 {
     public class Challenge
     {
-        public string[] GetData()
+
+        public static int FindSemiValidCount()
+        {
+            return ParseAllPassports().Count(passport => passport.IsValid());
+        }
+
+        public static Passport[] ParseAllPassports()
+        {
+            return ParseAllPassports(GetData());
+        }
+
+        public static Passport[] ParseAllPassports(string[] data)
+        {
+            var passports = new List<Passport>();
+            var lines = new List<string>();
+            foreach (var line in data)
+            {
+                if (line == string.Empty)
+                {
+                    passports.Add(ParsePassport(lines.ToArray()));
+                    lines = new List<string>();
+                }
+                else
+                {
+                    lines.Add(line);
+                }
+            }
+            return passports.ToArray();
+        }
+
+        public static Passport ParsePassport(string[] lines)
+        {
+            var fields = new List<string>();
+            foreach (var line in lines)
+            {
+                fields.AddRange(line.Split(" "));
+            }
+            var passport = new Passport();
+            foreach (var field in fields)
+            {
+                var split = field.Split(":");
+                var key = split[0];
+                var value = split[1];
+                switch (key)
+                {
+                    case "ecl":
+                        passport.Ecl = value;
+                        break;
+                    case "pid":
+                        passport.Pid = value;
+                        break;
+                    case "eyr":
+                        passport.Eyr = value;
+                        break;
+                    case "hcl":
+                        passport.Hcl = value;
+                        break;
+                    case "byr":
+                        passport.Byr = value;
+                        break;
+                    case "iyr":
+                        passport.Iyr = value;
+                        break;
+                    case "cid":
+                        passport.Cid = value;
+                        break;
+                    case "hgt":
+                        passport.Hgt = value;
+                        break;
+                }
+            }
+
+            return passport;
+        }
+
+        public static string[] GetData()
         {
             return File.ReadAllLines("Day4/Input.txt");
         }
@@ -21,21 +99,21 @@ namespace Year2020.Day4
         public string Pid { get; set; }
         public string Cid { get; set; }
 
-        public bool IsSemiValid()
-        {
-            return
-                (Byr != null) &&
-                (Iyr != null) &&
-                (Eyr != null) &&
-                (Hgt != null) &&
-                (Hcl != null) &&
-                (Ecl != null) &&
-                (Pid != null);
-        }
-
         public bool IsValid()
         {
-            return IsSemiValid() && Cid != null;
+            return
+                Byr != null &&
+                Iyr != null &&
+                Eyr != null &&
+                Hgt != null &&
+                Hcl != null &&
+                Ecl != null &&
+                Pid != null;
+        }
+
+        public bool IsFullyValid()
+        {
+            return IsValid() && Cid != null;
         }
         
     }

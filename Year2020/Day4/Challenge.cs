82a2376cd5c3818ae7c53645ed2f7826ca17c042
notThere0815 <klappstuhl@schoschonen.at>
2021-01-15 14:16:34 +0100

    Input for day4

--- Year2020/Day4/Challenge.cs
+++ Year2020/Day4/Challenge.cs
@@ -1,7 +1,43 @@
-﻿namespace Year2020.Day4
+﻿using System.IO;
+
+namespace Year2020.Day4
 {
     public class Challenge
     {
+        public string[] GetData()
+        {
+            return File.ReadAllLines("Day4/Input.txt");
+        }
+    }
+
+    public struct Passwort
+    {
+        public string Byr { get; set; }
+        public string Iyr { get; set; }
+        public string Eyr { get; set; }
+        public string Hgt { get; set; }
+        public string Hcl { get; set; }
+        public string Ecl { get; set; }
+        public string Pid { get; set; }
+        public string Cid { get; set; }
+
+        public bool IsSemiValid()
+        {
+            return
+                (Byr != null) &&
+                (Iyr != null) &&
+                (Eyr != null) &&
+                (Hgt != null) &&
+                (Hcl != null) &&
+                (Ecl != null) &&
+                (Pid != null);
+        }
+
+        public bool IsValid()
+        {
+            return IsSemiValid() && Cid != null;
+        }
         
     }
+    
 }

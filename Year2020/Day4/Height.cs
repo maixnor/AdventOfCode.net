c717e505a74d457c85333a52551992149cc740e6
notThere0815 <klappstuhl@schoschonen.at>
2021-01-18 13:09:36 +0100

    height to own file

---
+++ Year2020/Day4/Height.cs
@@ -0,0 +1,35 @@
+﻿namespace Year2020.Day4
+{
+    public struct Height
+    {
+        private int Number { get; init; }
+        private string Unit { get; init; }
+
+        public static Height ParseHeight(string height)
+        {
+            return new()
+            {
+                Number = int.Parse(height.Substring(0, height.Length - 2)),
+                Unit = height.Substring(height.Length - 2)
+            };
+        }
+
+        public override string ToString()
+        {
+            return Number + Unit;
+        }
+
+        public bool IsValid()
+        {
+            switch (Unit)
+            {
+                case "cm":
+                    return 150 <= Number && Number <= 193;
+                case "in":
+                    return 59 <= Number && Number <= 76;
+                default:
+                    return false;
+            }
+        }
+    }
+}
--- Year2020/Day4/Passport.cs
+++ Year2020/Day4/Passport.cs
@@ -151,32 +151,4 @@ namespace Year2020.Day4
         }
         
     }
-
-    public struct Height
-    {
-        private int Number { get; init; }
-        private string Unit { get; init; }
-
-        public static Height ParseHeight(string height)
-        {
-            return new()
-            {
-                Number = int.Parse(height.Substring(0, height.Length - 2)),
-                Unit = height.Substring(height.Length - 2)
-            };
-        }
-
-        public bool IsValid()
-        {
-            switch (Unit)
-            {
-                case "cm":
-                    return 150 <= Number && Number <= 193;
-                case "in":
-                    return 59 <= Number && Number <= 76;
-                default:
-                    return false;
-            }
-        }
-    }
 }

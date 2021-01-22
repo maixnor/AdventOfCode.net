75692aef1f6ee69b0cca61136f7afa159270ee0b
Benjamin Meixner <benjamin@meixnor.org>
2021-01-22 22:41:05 +0100

    parameters for size definition

--- Year2020/Day5/Challenge.cs
+++ Year2020/Day5/Challenge.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -5,6 +6,9 @@ namespace Year2020.Day5
 {
     public class Challenge
     {
+        private static readonly int PowerRows = 7; 
+        private static readonly int PowerCols = 3;
+
         public static IEnumerable<string> GetData()
         {
             return File.ReadLines("Day5/Input.txt");

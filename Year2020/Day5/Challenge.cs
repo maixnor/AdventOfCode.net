b0612d072a8c352607f786b5e4ae8fed3e7a8de0
Benjamin Meixner <benjamin@meixnor.org>
2021-01-22 22:31:19 +0100

    get data

--- Year2020/Day5/Challenge.cs
+++ Year2020/Day5/Challenge.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+using System.IO;
+
 namespace Year2020.Day5
 {
     public class Challenge
     {
-        
+        public static IEnumerable<string> GetData()
+        {
+            return File.ReadLines("Day5/Input.txt");
+        }
     }
 }

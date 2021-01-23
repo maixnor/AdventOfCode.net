36c7b91725748b27a8c9ae9d9e20bd0cf908a045
Benjamin Meixner <benjamin@meixnor.org>
2021-01-23 22:24:30 +0100

    IsValid was not a good idea

--- Year2020/Day5/Seat.cs
+++ Year2020/Day5/Seat.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Linq;
 
 namespace Year2020.Day5
 {
@@ -8,9 +6,7 @@ namespace Year2020.Day5
         public int Row { get; init; }
         public int Col { get; init; }
 
-        public bool IsValid => Row >= 0 && Col >= 0;
-
-        public int SeatId => IsValid ? Row * Challenge.Cols + Col : -1;
+        public int SeatId => Row * Challenge.Cols + Col;
         
     }
 }

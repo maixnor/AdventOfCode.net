1b4b03dc4a5428105a725063e004836deeb97805
Benjamin Meixner <benjamin@meixnor.org>
2021-01-23 09:10:43 +0100

    seat class for calculations

---
+++ Year2020/Day5/Seat.cs
@@ -0,0 +1,56 @@
+namespace Year2020.Day5
+{
+    public class Seat
+    {
+        private int RowUpperBound { get; set; }
+        private int RowLowerBound { get; set; }
+        private int ColUpperBound { get; set; }
+        private int ColLowerBound { get; set; }
+
+        public bool IsLimited => RowLowerBound == RowUpperBound && ColLowerBound == ColUpperBound;
+
+        public Seat(int rows, int cols)
+        {
+            RowUpperBound = rows;
+            ColUpperBound = cols;
+            RowLowerBound = 0;
+            ColLowerBound = 0;
+        }
+
+        public bool Limit(string limiters)
+        {
+            foreach (var limiter in limiters)
+            {
+                if (Limit(limiter))
+                    return true;
+            }
+            // could be replaced with false I think as the last limit call returns IsLimited,
+            // after the last limitation has taken place.
+            // would be beneficial to performance.
+            return IsLimited; 
+        }
+
+        public bool Limit(char limiter)
+        {
+            // limit upper bound and lower bound of rows and cols using the limiter
+            return IsLimited;
+        }
+
+        private void RowUpper()
+        {
+            
+        }
+        private void RowLower()
+        {
+            
+        }
+        private void ColUpper()
+        {
+            
+        }
+        private void ColLower()
+        {
+            
+        }
+    }
+}

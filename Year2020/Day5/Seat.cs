1086ec7565a7519fc6bb8aa9cb12b6c84e17d8e1
Benjamin Meixner <benjamin@meixnor.org>
2021-01-23 21:41:26 +0100

    stripping seat to whats necessary

--- Year2020/Day5/Seat.cs
+++ Year2020/Day5/Seat.cs
@@ -5,76 +5,10 @@ namespace Year2020.Day5
 {
     public struct Seat
     {
-        private int RowUpperBound { get; set; }
-        private int RowLowerBound { get; set; }
-        private int ColUpperBound { get; set; }
-        private int ColLowerBound { get; set; }
+        public static int Row { get; set; }
+        public static int Col { get; set; }
 
-        private int RowDiff => RowUpperBound - RowLowerBound;
-        private int ColDiff => ColUpperBound - ColLowerBound;
+        public static int SeatId => Row * Challenge.Cols + Col;
         
-        public bool IsLimited => RowLowerBound == RowUpperBound && ColLowerBound == ColUpperBound;
-
-        public Seat(string limiter)
-        {
-            RowUpperBound = Challenge.Rows - 1;
-            ColUpperBound = Challenge.Cols - 1;
-            RowLowerBound = 0;
-            ColLowerBound = 0;
-            Limit(limiter);
-        }
-
-        public Seat()
-        {
-            RowUpperBound = Challenge.Rows - 1;
-            ColUpperBound = Challenge.Cols - 1;
-            RowLowerBound = 0;
-            ColLowerBound = 0;
-        }
-
-        public int SeatId => IsLimited ? -1 : RowUpperBound * Challenge.Cols + ColUpperBound;
-
-        public bool Limit(string limiters)
-        {
-            return limiters.Any(Limit);
-        }
-
-        public bool Limit(char limiter)
-        {
-            switch (limiter)
-            {
-                // limit upper bound and lower bound of rows and cols using the limiter
-                case RowUpperChar:
-                    RowUpper();
-                    break;
-                case RowLowerChar:
-                    RowLower();
-                    break;
-                case ColUpperChar:
-                    ColUpper();
-                    break;
-                case ColLowerChar:
-                    ColLower();
-                    break;
-            }
-            return IsLimited;
-        }
-
-        private void RowUpper()
-        {
-            RowLowerBound += RowDiff / 2;
-        }
-        private void RowLower()
-        {
-            RowUpperBound -= RowDiff / 2;
-        }
-        private void ColUpper()
-        {
-            ColLowerBound += ColDiff / 2;
-        }
-        private void ColLower()
-        {
-            ColUpperBound -= ColDiff / 2;
-        }
     }
 }

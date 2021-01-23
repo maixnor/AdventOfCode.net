97251a688bd08fe9221e96326cd72c55b49275d5
Benjamin Meixner <benjamin@meixnor.org>
2021-01-23 16:31:02 +0100

    Parse all seats

--- Year2020/Day5/Challenge.cs
+++ Year2020/Day5/Challenge.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Year2020.Day5
 {
@@ -8,8 +9,14 @@ namespace Year2020.Day5
     {
         private const int PowerRows = 7;
         private const int PowerCols = 3;
+        
         public static int Rows => (int) Math.Pow(2, PowerRows);
         public static int Cols => (int) Math.Pow(2, PowerCols);
+
+        public static IEnumerable<Seat> ParseAllSeats()
+        {
+            return GetData().Select(line => new Seat(line));
+        } 
         
         public static IEnumerable<string> GetData()
         {

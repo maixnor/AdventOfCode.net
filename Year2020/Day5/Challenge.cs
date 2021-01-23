d958df2b467e0f85601fc4b984eb614ccccb657a
Benjamin Meixner <benjamin@meixnor.org>
2021-01-23 21:36:27 +0100

    method for finding my seat

--- Year2020/Day5/Challenge.cs
+++ Year2020/Day5/Challenge.cs
@@ -13,6 +13,19 @@ namespace Year2020.Day5
         public static int Rows => (int) Math.Pow(2, PowerRows);
         public static int Cols => (int) Math.Pow(2, PowerCols);
 
+        public static int YourSeatID()
+        {
+            var seats = ParseAllSeats().OrderBy(seat => seat.SeatId).ToArray();
+            // cycle from the 2nd to the 2nd to last seat (could not be first or last)
+            // and I do not have to check for OutOfBounds each cycle
+            for (int i = 1; i < seats.Length - 1; i++)
+            {
+                if (seats[i - 1].SeatId + 1 == seats[i].SeatId && seats[i].SeatId == seats[i + 1].SeatId - 1)
+                    return seats[i].SeatId;
+            }
+            return -1;
+        }
+
         public static IEnumerable<Seat> ParseAllSeats()
         {
             return GetData().Select(line => new Seat(line));

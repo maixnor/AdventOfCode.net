6da97082139e7ce849cbea9529786f55bc293fe5
Benjamin Meixner <benjamin@meixnor.org>
2021-01-24 21:20:38 +0100

    form simplified

--- Year2020/Day6/Form.cs
+++ Year2020/Day6/Form.cs
@@ -4,24 +4,16 @@ namespace Year2020.Day6
 {
     public struct Form
     {
-        public SortedSet<char> Checks { get; }
+        private SortedSet<char> Checks { get; }
 
-        public int ChecksCount()
-        {
-            return Checks.Count;
-        }
+        public int CheckCount => Checks.Count;
 
         public void Check(string checks)
         {
             foreach (var check in checks)
             {
-                Check(check);
+                Checks.Add(check);
             }
-        }
-
-        public void Check(char check)
-        {
-            Checks.Add(check);
         }
     }
 }

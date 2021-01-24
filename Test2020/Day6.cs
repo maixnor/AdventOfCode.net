3339dcb463baf194f2386459b7bbf08fe536c5ec
Benjamin Meixner <benjamin@meixnor.org>
2021-01-24 17:20:35 +0100

    GetData with Test

--- Test2020/Day6.cs
+++ Test2020/Day6.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+using Xunit;
+using Year2020.Day6;
+
 namespace Test2020
 {
     public class Day6
     {
-        
+        [Fact]
+        public void EnsureInput()
+        {
+            Assert.NotEmpty(Challenge.GetData());
+        }
     }
 }
--- Year2020/Day6/Challenge.cs
+++ Year2020/Day6/Challenge.cs
@@ -1,7 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
 namespace Year2020.Day6
 {
     public class Challenge
     {
-        
+        public static IEnumerable<string> GetData()
+        {
+            return File.ReadAllLines("Day6/Input.txt");
+        }
     }
 }

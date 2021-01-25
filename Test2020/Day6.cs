a4daff4940fc907b0f62650adbe41554f657d266
notThere0815 <klappstuhl@schoschonen.at>
2021-01-25 12:03:41 +0100

    new checks for new all methods

--- Test2020/Day6.cs
+++ Test2020/Day6.cs
@@ -7,10 +7,16 @@ namespace Test2020
     public class Day6
     {
         [Fact]
-        public void FindCombinedCheckCount()
+        public void FindCombinedAllCheckCount()
         {
-            _testOutputHelper.WriteLine(Challenge.GetCombinedCount().ToString());
+            _testOutputHelper.WriteLine(Challenge.GetCombinedCountAll().ToString());
         }
+        [Fact]
+        public void FindCombinedAnyCheckCount()
+        {
+            _testOutputHelper.WriteLine(Challenge.GetCombinedCountAny().ToString());
+        }
+
         [Fact]
         public void EnsureInput()
         {
@@ -18,24 +24,33 @@ namespace Test2020
         }
 
         [Fact]
-        public void EnsureForms()
+        public void EnsureFormAll()
         {
-            Assert.NotEmpty(Challenge.GetForms());
+            var form = new Form();
+            Assert.Equal(0, form.AllCheckCount);
+            form.AllCheck("abcx");
+            Assert.Equal(4, form.AllCheckCount);
+            form.AllCheck("abcy");
+            Assert.Equal(3, form.AllCheckCount);
+            form.AllCheck("abcz");
+            Assert.Equal(3, form.AllCheckCount);
+            form.AllCheck("1ASH");
+            Assert.Equal(0, form.AllCheckCount);
         }
 
         [Fact]
-        public void EnsureForm()
+        public void EnsureFormAny()
         {
             var form = new Form();
-            Assert.Equal(0, form.CheckCount);
+            Assert.Equal(0, form.AnyCheckCount);
             form.AnyCheck("abcx");
-            Assert.Equal(4, form.CheckCount);
+            Assert.Equal(4, form.AnyCheckCount);
             form.AnyCheck("abcy");
-            Assert.Equal(5, form.CheckCount);
+            Assert.Equal(5, form.AnyCheckCount);
             form.AnyCheck("abcz");
-            Assert.Equal(6, form.CheckCount);
+            Assert.Equal(6, form.AnyCheckCount);
             form.AnyCheck("1ASH");
-            Assert.Equal(6, form.CheckCount);
+            Assert.Equal(6, form.AnyCheckCount);
         }
         
         private readonly ITestOutputHelper _testOutputHelper;

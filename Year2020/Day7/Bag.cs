685b185e7248898252ccec560692a731124263d5
notThere0815 <klappstuhl@schoschonen.at>
2021-02-04 08:42:17 +0100

    Bag.Contains init => set

--- Year2020/Day7/Bag.cs
+++ Year2020/Day7/Bag.cs
@@ -5,7 +5,7 @@ namespace Year2020.Day7
     public struct Bag
     {
         public string Color { get; init; }
-        public Dictionary<Bag, int> Contains { get; init; }
+        public Dictionary<Bag, int> Contains { get; set; }
 
         public bool IsOfColor(string color)
         {

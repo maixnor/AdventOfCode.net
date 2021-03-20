dfe7a0246f286ee26d00dbafed86292be9b8ae88
maixnor <klappstuhl@schoschonen.at>
2021-03-20 21:32:55 +0100

    method extractions

--- Year2020/Day7/Challenge.cs
+++ Year2020/Day7/Challenge.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -21,17 +22,28 @@ namespace Year2020.Day7
             // [pale cyan bags contain 2 posh black bags, 4 wavy gold bags, 2 vibrant brown bags.]
             // split by "contain" => [pale cyan bags], [2 posh black bags, 4 wavy gold bags, 2 vibrant brown bags.]
             var parts = line.Split("contain");
+            var bagPart = parts[0];
             // parse bag with bag part
-            var bag = ParseBag(parts[0]);
+            var bag = ParseBag(bagPart);
             // split relation part by ',' => [2 posh black bags], [4 wavy gold bags], [2 vibrant brown bags.]
             // and parse to relations
-            foreach (var part in parts[1].Split(','))
-            {
-                var relation = ParseRelation(part);
-                if (relation is not null)
-                    bag.Contains.Add(relation.Value.Key, relation.Value.Value);
-            }
+            var relationsPart = parts[1];
+            AddRelationsToBag(relationsPart, bag);
             return bag;
+        }
+
+        private static void AddRelationsToBag(string relationsPart, Bag bag)
+        {
+            foreach (var relationStr in relationsPart.Split(','))
+            {
+                AddRelationToBagIfNotEmpty(ParseRelation(relationStr), bag);
+            }
+        }
+
+        private static void AddRelationToBagIfNotEmpty(KeyValuePair<Bag, int>? relation, Bag bag)
+        {
+            if (relation is not null)
+                bag.Contains.Add(relation.Value.Key, relation.Value.Value);
         }
 
         public static Bag ParseBag(string str)
@@ -50,10 +62,14 @@ namespace Year2020.Day7
                 return null;
             // split by ' ' once => [2], [posh black bags]
             var split = str.Trim().Split(' ', 2);
-            return new KeyValuePair<Bag, int>(
-                ParseBag(split[1]),
-                int.Parse(split[0])
-            );
+            
+            if (int.TryParse(split[0], out var number))
+                return new KeyValuePair<Bag, int>(
+                    ParseBag(split[1]),
+                    number
+                );
+
+            throw new Exception("This is not a valid relation string: "+str);
         }
         
         public static IEnumerable<string> GetData()
